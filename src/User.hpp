c30be51c36cd1b0338ef9cc9a539556f48e88a0a
Sakura_Dusk <sakura_dusk@sjtu.edu.cn>
2025-05-24 20:02:47 +0800

    add User.hpp

---
+++ src/User.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Sakura on 25-5-24.
+//
+
+#ifndef USER_HPP
+#define USER_HPP
+
+#include "FileStore-B+.hpp"
+
+class User {
+  private:
+    chars username, password, name, mailAddr;
+    int privilege;
+
+    User
+};
+
+
+
+#endif //USER_HPP
